2054d748e86a74459f13fee2ffe61bd07d589a88
David Baron <david@ndit.se>
2025-06-04 00:24:13 +0200

    fixed nvim config

---
+++ AppData/Local/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,19 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    code = {
+      sign = false,
+      width = "block",
+      right_pad = 1,
+    },
+    heading = {
+      sign = false,
+      icons = {},
+      width = "block",
+      min_width = 60,
+    },
+    checkbox = {
+      enabled = true,
+    },
+  },
+}
